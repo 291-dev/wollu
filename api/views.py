0fd25833d1187074ee51835799b97d38841f4431
JangYeHoon <jkjk128jkjk@gmail.com>
2023-01-02 23:12:01 +0900

    Add wollu monthly statistics data api

--- api/views.py
+++ api/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework import viewsets
 
 from .models import User, Wollu
-from .serializers import UserSerializer, WolluSerializer
+from .serializers import UserSerializer, WolluSerializer, WolluMonthSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -18,3 +18,13 @@ class UserViewSet(viewsets.ModelViewSet):
 class WolluViewSet(viewsets.ModelViewSet):
     queryset = Wollu.objects.all()
     serializer_class = WolluSerializer
+
+class WolluMonthView(APIView):
+    def get(self, request, user_id):
+        year = request.GET['year']
+        month = request.GET['month']
+        if None not in [user_id, year, month]:
+            return Response(WolluMonthSerializer(Wollu.objects.filter(user__exact=user_id,
+                        create_date__year=year, create_date__month=month).
+                        values('create_date', 'all_time'), many=True).data)
+        return Response(status=404)
