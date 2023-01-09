7205c48532202b14a2f243be67b1d53fabde1471
JangYeHoon <jkjk128jkjk@gmail.com>
2023-01-09 20:32:06 +0900

    Add basic ranking api

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
-from .views import ConnectionCheck, UserViewSet, WolluViewSet, WolluMonthView, StatsViewSet
+from .views import *
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet)
@@ -11,5 +11,6 @@ router.register('stats', StatsViewSet)
 urlpatterns = [
     path("test/", ConnectionCheck),
     path('wollu/month/<int:user_id>/', WolluMonthView.as_view()),
+    path('ranking/', WolluRankingView.as_view()),
     path('', include(router.urls))
 ]
